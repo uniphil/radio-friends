8c16776f4bd00f865dc07b96b4764d41f95bd4fe
phil <uniphil@gmail.com>
2017-10-12 01:13:53 -0400

    blah tweaks

--- src/CurrentlyPlaying.js
+++ src/CurrentlyPlaying.js
@@ -16,25 +16,26 @@ const CurrentlyPlaying = ({ muted, onToggleMute, track, next }) => (
           { track.name }
           <span className="CurrentlyPlaying-artist">{ track.artist }</span>
         </h4>
-        <p className="CurrentlyPlaying-queued">Queued by <span className="CurrentlyPlaying-queuer">{track.queuer}</span></p>
+        <p className="CurrentlyPlaying-queued">queued by <span className="CurrentlyPlaying-queuer">{track.queuer}</span></p>
       </div>
     </div>
     {next ? (
       <p className="CurrentlyPlaying-next">
-        Up next queued by{' '}
-        <span className="CurrentlyPlaying-next-queuer">
-          {`${next.queuer}: `}
-        </span>
+        up next:{' '}
         <span className="CurrentlyPlaying-next-title">
           {next.name}{' – '}
         </span>
         <span className="CurrentlyPlaying-next-artist">
-          {next.artist}{' '}
+          {next.artist}
+        </span>
+        , queued by{' '}
+        <span className="CurrentlyPlaying-next-queuer">
+          {next.queuer}
         </span>
       </p>
     ) : (
       <p className="CurrentlyPlaying-next">
-        Up next: <em>nothing queued yet!</em>
+        up next: <em>nothing queued yet!</em>
       </p>
     )}
   </div>
